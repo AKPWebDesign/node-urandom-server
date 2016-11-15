1c1cdde66ecac35ffebe526f25e7c8ad29199fa9
Austin Peterson <austin@akpwebdesign.com>
2016-11-15 16:05:05 -0600

    feat: configurable bytes.

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var e = require('express')
 var a = e()
 var p = process.env.PORT || 8080
 var r = new (require('randbytes')).urandom.getInstance()
+var b = process.env.BYTES || 5000;
 
 a.get('/', (req, res) => {
-  r.getRandomBytes(5000, (b) => {
+  r.getRandomBytes(b, (b) => {
     res.header('Content-Type', 'text/html')
     res.send(b)
   })
