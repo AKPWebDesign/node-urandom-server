952d008a11e41b602797f09cbd389e387a832776
Austin Peterson <austin@akpwebdesign.com>
2016-11-15 16:21:34 -0600

    feat: ability to request a number of bytes, up to a maximum limit.

--- server.js
+++ server.js
@@ -3,9 +3,26 @@ var a = e()
 var p = process.env.PORT || 8080
 var r = new (require('randbytes')).urandom.getInstance()
 var b = process.env.BYTES || 5000;
+var u = process.env.MAX_BYTES || 500000
 
 a.get('/', (req, res) => {
   r.getRandomBytes(b, (b) => {
+    res.header('Content-Type', 'text/html')
+    res.send(b)
+  })
+})
+
+a.get('/:b', (req, res) => {
+  let b2 = null;
+  try {
+    b2 = parseInt(req.params.b, 10);
+    if(!b2) { b2 = b }
+    b2 = Math.min(b2, u)
+  } catch (e) {
+    b2 = b
+  }
+
+  r.getRandomBytes(b2, (b) => {
     res.header('Content-Type', 'text/html')
     res.send(b)
   })
